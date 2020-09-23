4ee6af83fc2c7caa87e8366b50027459db56bf89
Verathor <shaywgroves@outlook.com>
2020-09-23 10:53:16 -0400

    .

--- win10debloatwork.ps1
+++ win10debloatwork.ps1
@@ -2806,6 +2806,7 @@ Function DebloatAll {
         "*Sway*"
         "*Speed Test*"
         "*Dolby*"
+	"*HP Support Assist*"
              
         #Optional: Typically not removed but you can if you need to for some reason
         "*Microsoft.Advertising.Xaml_10.1712.5.0_x64__8wekyb3d8bbwe*"
